7f99cbca3027a4666e95232d72409e5ace071620
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2022-12-15 16:45:56 -0700

    Create deltrack.sql

---
+++ models/example/deltrack.sql
@@ -0,0 +1,23 @@
+* Example tested Wild West BQ. 
+Setup your schema. For BigQuery table is my_dataset.my_new_table */
+INSERT INTO
+  elijah_onboarding_adoption_test.data_transform_sheet_1 (
+    linenumber,
+    _fivetran_synced,
+    uniqueid,
+    ftkey)
+/* Select the fields in the same order as the schema above from the orginal dataset
+The new primary key is a concatenation of the record values to detect changes
+ie object id will always be the same however airtype and airuse values associated 
+to the object id could change */
+SELECT
+   apt._line AS linenumber,
+   apt._fivetran_synced AS _fivetran_synced,
+   CONCAT(CAST(apt._line AS string),cast(apt.account_id as string),cast(apt.connection_method as string)) AS uniqueid,
+   CONCAT(CAST(apt._line AS string),cast(apt._fivetran_synced as string)) AS ftkey 
+FROM
+  fivetran-wild-west.elijah_onboarding_adoption_test.onboarding_and_adoption_tracker_account_checklist apt
+-- if concat key is not in new table then add
+WHERE
+  CONCAT(CAST(apt._line AS string),cast(apt.account_id as string),cast(apt.connection_method as string))
+     NOT IN (select dt.uniqueid from fivetran-wild-west.elijah_onboarding_adoption_test.data_transform_sheet_1 dt)
