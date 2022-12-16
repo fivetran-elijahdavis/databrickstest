252dee2a657fae39b10a850e7d1a339a517a31fa
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2022-12-15 17:02:00 -0700

    Update deltrack.sql

--- models/example/deltrack.sql
+++ models/example/deltrack.sql
@@ -1,13 +1,3 @@
-INSERT INTO
-  elijah_onboarding_adoption_test.data_transform_sheet_1 (
-    linenumber,
-    _fivetran_synced,
-    uniqueid,
-    ftkey)
-/* Select the fields in the same order as the schema above from the orginal dataset
-The new primary key is a concatenation of the record values to detect changes
-ie object id will always be the same however airtype and airuse values associated 
-to the object id could change */
 SELECT
    apt._line AS linenumber,
    apt._fivetran_synced AS _fivetran_synced,
