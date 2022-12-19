28a4817cc065e81c0c389261620ea2b372a8b701
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2022-12-19 15:11:10 -0700

    Update deltrack.sql

--- models/example/deltrack.sql
+++ models/example/deltrack.sql
@@ -12,10 +12,10 @@ SELECT
    apt._fivetran_synced AS _fivetran_synced,
    CONCAT(CAST(apt._line AS string),cast(apt.account_id as string),cast(apt.connection_method as string)) AS uniqueid,
    CONCAT(CAST(apt._line AS string),cast(apt._fivetran_synced as string)) AS ftkey
-   ,
-   CASE WHEN CONCAT(CAST(apt._line AS string),cast(apt.account_id as string),cast(apt.connection_method as string)) 
-   NOT IN (SELECT dt.uniqueid from fivetran-wild-west.elijah_dbt_databricks_test.deltrack dt) 
-   THEN 'Y' ELSE 'N'
-   END AS DeletedFlag
+   --,
+   --CASE WHEN CONCAT(CAST(apt._line AS string),cast(apt.account_id as string),cast(apt.connection_method as string)) 
+   --NOT IN (SELECT dt.uniqueid from fivetran-wild-west.elijah_dbt_databricks_test.deltrack dt) 
+   --THEN 'Y' ELSE 'N'
+   --END AS DeletedFlag
 FROM
   fivetran-wild-west.elijah_onboarding_adoption_test.onboarding_and_adoption_tracker_account_checklist apt
