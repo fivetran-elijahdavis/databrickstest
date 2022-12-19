a6acb8b0332adacdf6a4493f5484b21d9c66d84d
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2022-12-19 15:57:53 -0700

    Update deltrack.sql

--- models/example/deltrack.sql
+++ models/example/deltrack.sql
@@ -2,7 +2,6 @@ SELECT
    apt._line AS linenumber,
    apt._fivetran_synced AS _fivetran_synced,
    CONCAT(CAST(apt._line AS string),cast(apt.account_id as string),cast(apt.connection_method as string)) AS uniqueid,
-   dt.uniqueid as deltrackingid,
    CONCAT(CAST(apt._line AS string),cast(apt._fivetran_synced as string)) AS ftkey
   ,'N' as DeletedFlag
 
