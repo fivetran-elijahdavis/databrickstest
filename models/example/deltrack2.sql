1069afd0818980d19c91be075d02c93a0d754388
fivetran-elijahdavis <104081720+fivetran-elijahdavis@users.noreply.github.com>
2022-12-22 15:01:27 -0700

    Update deltrack2.sql

--- models/example/deltrack2.sql
+++ models/example/deltrack2.sql
@@ -1,14 +1,19 @@
-    SELECT
-    dt.linenumber,
-   dt._fivetran_synced,
-   dt.uniqueid,
-   case when dt.uniqueid
-    not in (select
-              CONCAT(CAST(apt._line AS string),cast(apt.account_id as string),cast(apt.connection_method as string ))
-            from  fivetran-wild-west.elijah_onboarding_adoption_test.onboarding_and_adoption_tracker_account_checklist apt)
-            then 'Y' else 'N'
-            end as deletedflag
-   ,
-   dt.ftkey
+---------Load audit table
+insert into fivetran-wild-west.elijah_dbt_databricks_test.deltrack (uniqueid,deletedflag)
+
+with a
+as (
+select uniqueid,deletedflag from fivetran-wild-west.elijah_dbt_databricks_test.deltrack dt
+),
+b as(
+select CONCAT(CAST(apt._line AS string),cast(apt.account_id as string),cast(apt.connection_method as string)) AS loadeduniqueid
 FROM
-fivetran-wild-west.elijah_dbt_databricks_test.deltrack dt
+  fivetran-wild-west.elijah_onboarding_adoption_test.onboarding_and_adoption_tracker_account_checklist apt
+),
+c as(
+select loadeduniqueid as newids,'New' as deletedflag from b where b.loadeduniqueid not in (select a.uniqueid from a)
+),
+d as(
+select a.uniqueid as delids, 'Y' as deletedflag from a where a.uniqueid not in (select loadeduniqueid from b)
+)
+select * from c
